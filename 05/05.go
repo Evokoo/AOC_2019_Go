171a6ea01474f32be9ede1a33deac385e8afd4d8
Evokoo <evanjohncooper@gmail.com>
2025-10-11 01:12:45 +1100

    Day 05 complete

--- 05/05.go
+++ 05/05.go
@@ -4,11 +4,15 @@ import (
 	"github.com/Evokoo/AOC_2019_Go/intcode"
 )
 
-func Solve(file string) int {
-	program := ParseInput(file)
-	cpu := intcode.NewCPU(program)
+func Solve(file string, part int) int {
+	cpu := intcode.NewCPU(ParseInput(file))
+	id := 1
 
-	cpu.WriteToInput(1)
+	if part == 2 {
+		id = 5
+	}
+
+	cpu.PushToInput(id)
 	cpu.Run()
 	output := cpu.GetOutput()
 
